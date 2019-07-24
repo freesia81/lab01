d8a28642ed37c4a8c140c887032f8890411311ff
freesia81 <51009145+freesia81@users.noreply.github.com>
2019-07-24 09:44:22 +0900

    Add files via upload

---
+++ biz_remove_ext.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define MAX_DATA_SIZE   20
+#define MAX_DATA_LENGTH 20
+
+int main(int argc, char *argv[])
+{
+	char fNameList[MAX_DATA_SIZE][MAX_DATA_LENGTH] = { "abc.txt", "abce", "abcd.txt", "acde.txt"};
+	int fNameCnt = 4;
+
+	for(int i=0; i<fNameCnt; i++)
+	{
+		char buf[MAX_DATA_LENGTH] = {0, };
+		char *ptr;
+
+		// 파일명 확장자 제거
+		memset(buf, 0x00, sizeof(buf));
+		strcpy(buf, fNameList[i]);
+		if ( (ptr = strchr(buf, '.')) != NULL )
+			*ptr = 0x00;
+
+		printf("%s\n", buf);
+	}
+
+	return 0;
+}
+
+
